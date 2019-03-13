a2f830b11b4091fd0ac129e19e496f1867168cac
Serere Jegede <sererejegede@gmail.com>
2019-03-13 23:05:00 +0100

    Almost done with mockup2

---
+++ paychoice-gate.js
@@ -0,0 +1,28 @@
+window.onload = () => {
+  var paychoiceGate = new Vue({
+    el: '#paychoice-gate',
+    data: {
+      current_tab: 'tab-one',
+      current_stage: 'three'
+    },
+    computed: {
+      
+    },
+    methods: {
+      switchTab(tab) {
+        this.current_tab = tab;
+      },
+      currentTab(tab) {
+        return {
+          'shadow': this.current_tab === tab,
+          'active-tab': this.current_tab === tab
+        }
+      }
+    },
+    mounted() {
+      console.log('Vue initialized');
+    }
+  })
+}
+
+console.log('We are here');
